f0e0d60af60c96ffcb077106e69a68e56172562f
emelkin-pro <emelkin-pro@yandex.ru>
2021-09-03 07:35:12 +0300

    1.6. Взаимодействие объектов

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryEscape(Hare hare) {
+    }
+    public void tryEscape(Wolf wolf) {
+    }
+    public void tryEscape(Fox fox) {
+    }
+}
