3522f42f714dc1f2209ccd6e38163fff352b19e1
emelkin <kfsdkjfhkjsdhfljashdfk@fdfhksdfhsdfsjk>
2021-12-12 20:40:27 +0300

    6. Date. Отображение даты.

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item(){
     }
@@ -31,5 +32,13 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated(){
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
     }
 }
