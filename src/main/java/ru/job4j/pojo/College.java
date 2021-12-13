52b4360c88ed1ff41af97e1138b415ee3244f53c
emelkin <kfsdkjfhkjsdhfljashdfk@fdfhksdfhsdfsjk>
2021-12-13 14:23:12 +0300

    2. Модель данных.

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.pojo;
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Емельянов Олег");
+        student.setGroup("БИ211");
+        student.setDateOfAdmission(new Date());
+
+        System.out.println("Студент: " + student.getFio() + " Зачислен в группу " + student.getGroup() + ", дата зачисления: " + student.getDateOfAdmission());
+    }
+}
