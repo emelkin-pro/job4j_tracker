dac6fee09cf5e8065a55d5d934ebb92b1f5f1a8e
emelkin <kfsdkjfhkjsdhfljashdfk@fdfhksdfhsdfsjk>
2021-12-14 15:12:21 +0300

    5. Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,25 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] newItems = new Item[items.length];
+        int newItemSize = 0;
+        for (Item item :
+                items) {
+            if (item != null) {
+                if (key.equals(item.getName())) {
+                    newItems[newItemSize] = item;
+                    newItemSize++;
+                }
+            }
+        }
+        return Arrays.copyOf(newItems, newItemSize);
     }
 
     public Item findById(int id) {
