e60ccacd2a4a4fd5c5831e3b16a2d33e32b851db
emelkin <kfsdkjfhkjsdhfljashdfk@fdfhksdfhsdfsjk>
2021-12-15 10:00:06 +0300

    5. Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -18,18 +18,14 @@ public class Tracker {
     }
 
     public Item[] findByName(String key) {
-        Item[] newItems = new Item[items.length];
-        int newItemSize = 0;
-        for (Item item :
-                items) {
-            if (item != null) {
-                if (key.equals(item.getName())) {
-                    newItems[newItemSize] = item;
-                    newItemSize++;
-                }
+        Item[] result = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (key.equals(items[i].getName())) {
+                result[count++] = items[i];
             }
         }
-        return Arrays.copyOf(newItems, newItemSize);
+        return Arrays.copyOf(result, count);
     }
 
     private int indexOf(int id) {
