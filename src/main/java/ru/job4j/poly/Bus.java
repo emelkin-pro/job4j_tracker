b84b0115c538b01f8cb6221c76117a2c2ddbd0f5
emelkin <kfsdkjfhkjsdhfljashdfk@fdfhksdfhsdfsjk>
2021-12-22 14:25:12 +0300

    3. Интерфейс Input

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,18 +1,23 @@
 package ru.job4j.poly;
 
 public class Bus implements Transport{
+    private int passengers ;
+    private float fuel;
+    private boolean drive = false;
+
     @Override
     public void drive() {
-
+        drive = !drive;
     }
 
     @Override
     public void passengers(int passengers) {
-
+        this.passengers = passengers;
     }
 
     @Override
     public float refuel(float fuel) {
-        return 0;
+        this.fuel = fuel;
+        return fuel * 1.5f;
     }
 }
