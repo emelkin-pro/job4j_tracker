e14f163dec4cad052de8d13266e7e070f76849cb
emelkin <kfsdkjfhkjsdhfljashdfk@fdfhksdfhsdfsjk>
2022-05-20 12:45:24 +0300

    0.4. Пользовательские исключения.

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,21 +4,21 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (value[i] == key) {
+            if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
         }
         if (rsl == -1) {
             throw new ElementNotFoundException("Element have not been found");
-        } else {
-            return rsl;
         }
+            return rsl;
     }
 
     public static void main(String[] args) {
         String[] array = {"Вася", "Петя", "Коля", "Маша"};
         try {
-            indexOf(array, "Маша");
+            indexOf(array, "Маша1");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
