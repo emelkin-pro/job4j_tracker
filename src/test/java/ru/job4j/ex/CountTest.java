b8a92bfa8ec5917440db8733e7ca37162ee7f2bf
emelkin <kfsdkjfhkjsdhfljashdfk@fdfhksdfhsdfsjk>
2022-07-28 08:33:45 +0300

    2.1. Тест на ValidateInput

--- src/test/java/ru/job4j/ex/CountTest.java
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -9,12 +9,11 @@ import org.junit.jupiter.api.Test;
 public class CountTest {
 
     @Test
-    public void whenException() {
-        IllegalArgumentException exception = assertThrows(
-                IllegalArgumentException.class,
-                () -> {
-                    Count.add(10, 2);
-                });
-        assertThat(exception.getMessage()).isEqualTo("Start should be less than finish.");
+    public void whenStart1Finish10IsSum44() {
+        int start = 0;
+        int finish = 3;
+        int expected = 3;
+        int result = Count.add(start, finish);
+        assertThat(result).isEqualTo(expected);
     }
 }
