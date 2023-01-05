caf05c2b0b84eca1a6516e73aa12fe5391d6b945
emelkin-pro <emelkin-pro@yandex.ru>
2023-01-05 12:02:46 +0300

    2. Идентичные тексты

---
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -0,0 +1,35 @@
+package ru.job4j.queue;
+
+import java.util.Queue;
+
+public class AppleStore {
+    private final Queue<Customer> queue;
+
+    private final int count;
+
+    public AppleStore(Queue<Customer> queue, int count) {
+        this.queue = queue;
+        this.count = count;
+    }
+
+    public String getLastHappyCustomer() {
+        for (int i = 0; i < count; i++) {
+            if (i == count - 1) {
+                return queue.poll().name();
+            }
+            queue.poll();
+        }
+        return "";
+    }
+
+    public String getLastUpsetCustomer() {
+        for (int i = 0; i < count; i++) {
+            if (i == count - 1) {
+                queue.poll();
+                return queue.poll().name();
+            }
+            queue.poll();
+        }
+        return "";
+    }
+}
