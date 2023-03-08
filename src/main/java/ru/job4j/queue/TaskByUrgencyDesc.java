1f44f543c46a92733c2d6b29ec69b53dd2d54f01
emelkin-pro <emelkin-pro@yandex.ru>
2023-03-08 14:17:23 +0300

    5. PriorityQueue

--- src/main/java/ru/job4j/queue/TaskByUrgencyDesc.java
+++ src/main/java/ru/job4j/queue/TaskByUrgencyDesc.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class TaskByUrgencyDesc implements Comparator<Task> {
     @Override
     public int compare(Task o1, Task o2) {
-        return Integer.valueOf(o2.urgency()).compareTo(Integer.valueOf(o1.urgency())) ;
+        return Integer.valueOf(o2.urgency()).compareTo(Integer.valueOf(o1.urgency()));
     }
 }
