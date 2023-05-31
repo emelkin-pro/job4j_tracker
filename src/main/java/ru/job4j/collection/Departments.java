dcccc685aa86d3b402b92d01b96597e81fc82f49
emelkin-pro <emelkin-pro@yandex.ru>
2023-05-31 12:47:30 +0500

    3. Отсортировать департаменты

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -1,9 +1,6 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.LinkedHashSet;
+import java.util.*;
 
 public class Departments {
 
@@ -25,8 +22,11 @@ public class Departments {
     }
 
     public static void sortAsc(List<String> orgs) {
+        orgs.sort(Comparator.naturalOrder());
     }
 
     public static void sortDesc(List<String> orgs) {
+        DepDescComp descComparator = new DepDescComp();
+        orgs.sort(descComparator);
     }
 }
