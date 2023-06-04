e653e7baede5fa2afe70628f1b42b77d4d2fd173
emelkin-pro <emelkin-pro@yandex.ru>
2023-06-04 13:38:07 +0500

    2.2. Функциональный интерфейс

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -18,7 +18,7 @@ public class FunctionalInterfaces {
         biCon.accept(6, "six");
         biCon.accept(7, "seven");
 
-        BiPredicate<Integer, String> biPred = (s1, s2) -> s1 % 2 == 0 || s2.length() == 4;
+        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || s.length() == 4;
         for (Integer i : map.keySet()) {
             if (biPred.test(i, map.get(i))) {
                 System.out.println("key: " + i + " value: " + map.get(i));
