15b2c98b2b45e37fdb32f52b0f49921faa8d3c62
emelkin-pro <emelkin-pro@yandex.ru>
2023-06-04 11:07:42 +0500

    2.3. Встроенные функциональные интерфейсы.

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -3,13 +3,11 @@ package ru.job4j.function;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 public class SearchFolder {
 
     public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
-        Supplier<List<Folder>> sup = () -> new ArrayList<>();
-        List<Folder> rsl = sup.get();
+        List<Folder> rsl = new ArrayList<>();
         for (Folder f : list) {
             if (pred.test(f)) {
                 rsl.add(f);
