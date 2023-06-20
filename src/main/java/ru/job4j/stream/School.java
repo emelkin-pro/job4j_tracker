b6a9d34ed80ba92719e8ca517571c25f84b27c73
emelkin-pro <emelkin-pro@yandex.ru>
2023-06-20 11:32:46 +0300

    5. Фильтрация учеников

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
         return students.stream()
