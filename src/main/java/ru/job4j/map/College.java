2bca4a840f9173bca616459bdc89ef6bfa677548
emelkin-pro <emelkin-pro@yandex.ru>
2023-07-05 17:34:56 +0300

    3. Optional - empty, of

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -1,6 +1,7 @@
 package ru.job4j.map;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 public class College {
@@ -11,37 +12,18 @@ public class College {
         this.students = students;
     }
 
-    public Student findByAccount(String account) {
+    public Optional<Student> findByAccount(String account) {
         return students.keySet()
                 .stream()
                 .filter(s -> s.account().equals(account))
-                .findFirst()
-                .orElse(null);
+                .findFirst();
     }
 
-    public Subject findBySubjectName(String account, String name) {
-        Student a = findByAccount(account);
-        if (a != null) {
-            return students.get(a)
-                    .stream()
-                    .filter(s -> s.name().equals(name))
-                    .findFirst()
-                    .orElse(null);
-        }
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Map<Student, Set<Subject>> students = Map.of(new Student("Student", "000001", "201-18-15"),
-                Set.of(
-                        new Subject("Math", 70),
-                        new Subject("English", 85)
-                )
-        );
-        College college = new College(students);
-        Student student = college.findByAccount("000001");
-        System.out.println("Найденный студент: " + student);
-        Subject english = college.findBySubjectName("000001", "English");
-        System.out.println("Оценка по найденному предмету: " + english.score());
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        Optional<Student> a = findByAccount(account);
+        return a.flatMap(student -> students.get(student)
+                .stream()
+                .filter(s -> s.name().equals(name))
+                .findFirst());
     }
 }
