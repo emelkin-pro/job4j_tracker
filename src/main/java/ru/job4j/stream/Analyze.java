d2c86ab3d733a96847ba9782435ce951583b6521
emelkin-pro <emelkin-pro@yandex.ru>
2023-09-28 13:27:00 +0300

    3. Аттестация FP, Lambda, Stream API

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,58 @@
+package ru.job4j.stream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+
+public class Analyze {
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(x -> x.subjects().stream())
+                .mapToInt(Subject::score)
+                .average()
+                .orElse(0);
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.map(x -> new Tuple(x.name(),
+                        x.subjects().stream()
+                                .mapToInt(Subject::score)
+                                .average()
+                                .orElse(0)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+
+        return stream.flatMap(s -> s.subjects().stream())
+                .collect(Collectors.groupingBy(Subject::name,
+                        LinkedHashMap::new,
+                        Collectors.averagingDouble(Subject::score)))
+                .entrySet().stream()
+                .map(p -> new Tuple(p.getKey(), p.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        CompareTupleMax compareTupleMax = new CompareTupleMax();
+        return stream.map(x -> new Tuple(x.name(), x.subjects()
+                        .stream()
+                        .mapToInt(Subject::score)
+                        .sum()))
+                .max(compareTupleMax)
+                .orElse(new Tuple("No name", 0));
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        CompareTupleMax compareTupleMax = new CompareTupleMax();
+        return stream.flatMap(s -> s.subjects().stream())
+                .collect(Collectors.groupingBy(Subject::name,
+                        LinkedHashMap::new,
+                        Collectors.summingDouble(Subject::score)))
+                .entrySet().stream()
+                .map(p -> new Tuple(p.getKey(), p.getValue()))
+                .max(compareTupleMax)
+                .orElse(new Tuple("No name", 0));
+    }
+}
